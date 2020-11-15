5ffe1c07aaea3faad9e49ee740b092ace913db9b
briansimiyuj <briansimiyuj@gmail.com>
2020-11-15 10:46:10 +0300

    Getting the DOM elements

---
+++ script.js
@@ -0,0 +1,19 @@
+const output = document.querySelector('.output')
+
+const lengthEl = document.querySelector('#length')
+
+const upperCase = document.querySelector('#uppercase')
+
+const lowerCase = document.querySelector('#lowercase')
+
+const numbers = document.querySelector('#numbers')
+
+const symbolEL= document.querySelector('#symbols')
+
+const generate = document.querySelector('#generate')
+
+const clipboard = document.querySelector('.clipboard')
+
+
+
+// console.log(output, lengthEl, upperCase, lowerCase, numbers, symbolEL, generate, clipboard)
